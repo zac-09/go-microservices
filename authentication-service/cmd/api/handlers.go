36e3eb480bf84aaaf96a7d5cf06c2de1268396df
isaac <isaacmubiru99@gmail.com>
2022-10-24 07:55:00 +0300

    feat(service): add logger service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -13,6 +16,8 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		Passowrd string `json:"password"`
 	}
 	err := app.readJSON(w, r, &requestPayload)
+	log.Println("hit here with1", err, requestPayload)
+
 	if err != nil {
 
 		app.errorJSON(w, err, http.StatusBadRequest)
@@ -27,17 +32,48 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, errors.New("invalid crendentials"), http.StatusBadRequest)
 		return
 	}
+	log.Println("hit here with2", user, err)
+
 	valid, err := user.PasswordMatches(requestPayload.Passowrd)
+	log.Println("hit here with", valid, err)
 	if err != nil || !valid {
 
 		app.errorJSON(w, errors.New("invalid crendentials"), http.StatusBadRequest)
 		return
 	}
+	// log auth
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
+	// log authentication
 	paylod := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
 		Data:    user,
 	}
 	app.writeJSON(w, http.StatusAccepted, paylod)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+	entry.Name = name
+	entry.Data = data
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceURL := "http://logger-service/log"
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -4,17 +4,23 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"log"
 	"net/http"
 )
 
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Passowrd string `json:"password"`
+}
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -29,19 +35,23 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	var requestPayload RequestPayload
 
 	err := app.readJSON(w, r, &requestPayload)
+	log.Println("error is", err, requestPayload)
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
 	switch requestPayload.Action {
 	case "auth":
-		app.authemticate(w, requestPayload.Auth)
+		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unkown actions"))
+
 	}
 }
 
-func (app *Config) authemticate(w http.ResponseWriter, a AuthPayload) {
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create json to send to auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
@@ -85,5 +95,33 @@ func (app *Config) authemticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Error = false
 	payload.Message = "Authenticated"
 	payload.Data = jsonFromService.Data
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
